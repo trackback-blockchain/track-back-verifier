70a5d578e7adf50577b684cee65753569da01b31
Gayan <gayan@track-back.co>
2021-08-16 16:09:13 +1200

    VCP fixes

--- demo-verifier/src/App.js
+++ demo-verifier/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 var QRCode = require('qrcode.react');
 
+// var QRCode = require('react-rq-code');
+
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <QRCode value="https://verifier.trackback.dev/api/v1/vcp" size={400} />
-      </header>
-      <div className="details"></div>
+      <div className="App-title">TrackBack™ Verifier</div>
+      <div className="App-header"> 
+        {/* <QRCode value="https://verifier.trackback.dev/api/v1/vcp" size={400} /> */}
+        <QRCode value="https://a684b174e0d5.ngrok.io/api/v1/vcp" size={500} className="QR-Scanner"/>
+      </div>
+      <div className="details">
+        <div className="sharedCredentials">
+          Shared credentials
+        </div>
+        <div className="verifiedCredentials">
+          Verified credentials
+        </div>
+      </div>
     </div>
   );
 }
